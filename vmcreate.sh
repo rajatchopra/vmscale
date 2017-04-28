1c1d1caffbb325ede0304a725ec9d2dded4dd7fb
Rajat Chopra <rchopra@redhat.com>
2017-04-27 20:44:51 -0400

    change directory back to root

--- vmcreate.sh
+++ vmcreate.sh
@@ -33,4 +33,5 @@ do
   genisoimage -output ${NODE_NAME}_cloud-init.iso -volid cidata -joliet -rock user-data meta-data
   virsh vol-create-as ovn-kubernetes ${NODE_NAME}.qcow2 20G --format qcow2 --backing-vol ../CentOS-7-x86_64-GenericCloud.qcow2 --backing-vol-format qcow2
   virt-install --connect qemu:///system --ram 10240 -n ${NODE_NAME} --os-type=linux --os-variant=rhel7  --disk path=${STORAGE_PATH}/${NODE_NAME}_config/${NODE_NAME}.qcow2,device=disk,bus=virtio,format=qcow2 --disk path=${STORAGE_PATH}/${NODE_NAME}_config/${NODE_NAME}_cloud-init.iso,device=cdrom,bus=virtio,format=iso --vcpus=2 --graphics spice --noautoconsole --import
+  cd ..
 done
