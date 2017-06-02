f2c08a1531b3bd62ddc0189e39d3b76f20a63e4e
Rajat Chopra <rchopra@redhat.com>
2017-06-02 18:51:08 +0000

    fixed cleanup script

--- vmclean.sh
+++ vmclean.sh
@@ -6,10 +6,6 @@ mkdir -p libvirt_storage
 cd libvirt_storage
 export STORAGE_PATH=`pwd`
 
-## Delete the storage pool
-virsh pool-destroy ovn-kubernetes
-virsh pool-delete ovn-kubernetes
-
 MACHINE_PREFIX=`hostname`
 for i in `seq 1 ${NUM_VMS_PER_MACHINE}`
 do
@@ -18,4 +14,14 @@ do
   virsh undefine ${NODE_NAME}
 done
 
+cd ..
+rm -rf ${STORAGE_PATH}
+
+## Delete the storage pool
+virsh pool-destroy ovn-kubernetes
+virsh pool-delete ovn-kubernetes
+
+#brctl delbr rcbr0
+brctl delif rcbr0 em1
+ip link set dev rcbr0 down
 brctl delbr rcbr0
