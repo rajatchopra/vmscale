9413421ed76a2c5a377ca1c627f9064509c59ef8
Rajat Chopra <rchopra@redhat.com>
2017-06-02 01:42:57 -0400

    vol-create path fix

--- vmcreate.sh
+++ vmcreate.sh
@@ -30,10 +30,10 @@ do
   cd ${NODE_NAME}_config
   cp ../../user_data .
   cp ../../meta_data .
-  sed -i "/NODE_NAME/${NODE_NAME}" user_data
-  sed -i "/NODE_NAME/${NODE_NAME}" meta_data
-  genisoimage -output ${NODE_NAME}_cloud-init.iso -volid cidata -joliet -rock user-data meta-data
-  virsh vol-create-as ovn-kubernetes ${NODE_NAME}.qcow2 20G --format qcow2 --backing-vol ../CentOS-7-x86_64-GenericCloud.qcow2 --backing-vol-format qcow2
+  sed -i s/NODE_NAME/${NODE_NAME}/ user_data
+  sed -i s/NODE_NAME/${NODE_NAME}/ meta_data
+  genisoimage -output ${NODE_NAME}_cloud-init.iso -volid cidata -joliet -rock user_data meta_data
+  virsh vol-create-as ovn-kubernetes ${NODE_NAME}.qcow2 20G --format qcow2 --backing-vol ${STORAGE_PATH}/CentOS-7-x86_64-GenericCloud.qcow2 --backing-vol-format qcow2
   virt-install --connect qemu:///system --ram 10240 -n ${NODE_NAME} --os-type=linux --os-variant=rhel7  --disk path=${STORAGE_PATH}/${NODE_NAME}_config/${NODE_NAME}.qcow2,device=disk,bus=virtio,format=qcow2 --disk path=${STORAGE_PATH}/${NODE_NAME}_config/${NODE_NAME}_cloud-init.iso,device=cdrom,bus=virtio,format=iso --vcpus=2 --graphics spice --noautoconsole --import
   cd ..
 done
