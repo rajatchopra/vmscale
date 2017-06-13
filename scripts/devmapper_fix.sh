e9bc28c56f6bbd439e49801a36ad75291dee4e42
Rajat Chopra <rchopra@redhat.com>
2017-06-13 02:53:42 +0000

    sar idle data; devmapper fix

---
+++ scripts/devmapper_fix.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+sudo systemctl stop origin-node
+sudo systemctl stop docker
+sudo dmsetup remove_all
+sudo dmsetup udevcomplete_all
+sudo systemctl start docker
+sudo systemctl start origin-node
